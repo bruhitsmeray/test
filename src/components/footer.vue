079f3520cc9430a6617dd09688b15861a0dab944
Whiteline <48377708+xWhiteline@users.noreply.github.com>
2021-01-16 13:06:56 +0200

    added files.

---
+++ src/components/footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id = "footer">
+        <footer>
+            <p>&copy; MORAR ANDREI - ALEXANDRU</p>
+            <p>2019-2021</p>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'footer.vue'
+    };
+</script>
+
+<style scoped>
+    footer {
+    position: relative;
+    bottom: 0;
+
+    width: 100%;
+    padding: 2.5rem;
+    color: white;
+    }
+
+    footer p {
+        color: black;
+    }
+</style>
---
+++ src/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <navbar />
+      <showcase />
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+  import navbar from './components/navbar.vue'
+  import showcase from './components/showcase.vue'
+  import Footer from './components/footer.vue'
+
+  export default {
+    name: 'index',
+    components: {
+      navbar,
+      showcase,
+      Footer
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    line-height: 1.4;
+    background: whitesmoke;
+
+    color: #333;
+    overflow-x: hidden;
+  }
+
+  * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+  }
+
+  .container {
+    max-width: 1100px;
+    margin: auto;
+}
+</style>
